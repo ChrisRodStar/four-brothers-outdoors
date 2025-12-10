c65892fe45b56c26bbcc020561b0b4734d5e8958
ChrisRodStar <chris.rodriguez.code@gmail.com>
2025-12-09 22:24:47 -0500

    testing github stauts

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,17 @@
-import Footer from "@/components/Footer";
-
-import AboutUs from "@/components/AboutUs";
-import Services from "@/components/Services";
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import AboutUs from "@/components/AboutUs";
+import Footer from "@/components/Footer";
+
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <Navigation />
       <Hero />
-
       <Services />
-
       <AboutUs />
-
-
-
       <Footer />
     </div>
   );
